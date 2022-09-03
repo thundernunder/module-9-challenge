4c0b7045e2d48f51d871b5f26c3a63f0fc912e9f
Nunder <nathannunderwood@gmail.com>
2022-09-02 22:12:13 -0700

    prompts built need to write file to fs

--- index.js
+++ index.js
@@ -1,89 +1,75 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 // TODO: Create an array of questions for user input
 const questions = [
-    "1. What is your project title?",
-    "2. Please provide a description of your project.", 
-    "3. Please provide useage information.", 
-    "4. Please provide contribution guidelines.", 
-    "5. Please provide test instructions."
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is your project title?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: "Please provide a description of your project.",
+  },
+  {
+    type: 'input',
+    name: 'useage',
+    message: "Please provide useage information.",
+  },
+  {
+    type: 'input',
+    name: 'contribution',
+    message: "Please provide contribution guidelines.",
+  },
+  {
+    type: 'input',
+    name: 'test',
+    message: "Please provide test instructions.",
+  },    
+  {
+    type: 'list',
+    name: 'licenses',
+    message: 'What license would you like to include?',
+    choices: ['MIT', 'GPL', 'CC--0']
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Enter your GitHub URL.',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email.',
+  },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, title, description, useage, contricution, test, github, email) {
-    `<!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-      <title>Document</title>
-    </head>
-    <body>
-      <div class="jumbotron jumbotron-fluid">
-      <div class="container">
-        <h1 class="display-4">${title}</h1>
-        <p class="lead">${description}.</p>
-        <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-        <ul class="list-group">
-          <li class="list-group-item">My GitHub username is ${github}</li>
-          <li class="list-group-item">LinkedIn: ${email}</li>
-        </ul>
-      </div>
-    </div>
-    </body>
-    </html>`
-
-    inquirer
-    .prompt([
-        {
-        type: 'input',
-        name: 'title',
-        message: 'What is your project title?',
-        },
-        {
-        type: 'input',
-        name: 'description',
-        message: "Please provide a description of your project.",
-        },
-        {
-        type: 'input',
-        name: 'useage',
-        message: "Please provide useage information.",
-        },
-        {
-        type: 'input',
-        name: 'contribution',
-        message: "Please provide contribution guidelines.",
-        },
-        {
-        type: 'input',
-        name: 'test',
-        message: "Please provide test instructions.",
-        },
-        {
-        type: 'input',
-        name: 'github',
-        message: 'Enter your GitHub URL.',
-        },
-        {
-        type: 'input',
-        name: 'email',
-        message: 'Enter your email.',
-        }
-    ])
-    .then((answers) => {
-        const mdPageContent = generate(answers);
-
-        fs.writeFile('README.md', mdPageContent, (err) =>
-        err ? console.log(err) : console.log('Successfully created README.md!')
-        );
-    });
+const writeToFile = data => {
+  return new Promise((resolve, reject) => {
+      // make a readme file and add to utils folder
+      fs.writeFile('./utils/newREADME.md', data, err => {
+          if (err) {
+              reject (err);
+              return;
+          }
+          resolve({
+              ok: true,
+              message: console.log('You have successfully created your README!')
+          });
+      })
+  })
 }
 
-// // TODO: Create a function to initialize app
-// function init() {}
+
+// TODO: Create a function to initialize app
+function init() {
+  inquirer.prompt(questions);
+}
 
 // // Function call to initialize app
-// init();
+init()
+
